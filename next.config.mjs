0a5a3dfb1a30ae7c726f82e1edc66b3bd3813551
ITExpertNow <152658834+itexpertnow@users.noreply.github.com>
2024-05-14 21:52:28 +0100

    up

--- next.config.mjs
+++ next.config.mjs
@@ -2,20 +2,21 @@ import withPWAInit from "next-pwa";
 
 const withPWA = withPWAInit({
   dest: "public",
-  disable: process.env.NODE_ENV === 'development',
-  register: true,
-  skipWaiting: true,
-  sw: "/static/sw.js",
-  buildExcludes: [/middleware-manifest.json$/],
-  cacheOnFrontEndNav: true,
-  reloadOnOnline: true,
-  fallbacks: {
-    //image: "/static/images/fallback.png",
-    document: "/offline", // if you want to fallback to a custom page rather than /_offline
-    // font: '/static/font/fallback.woff2',
-    // audio: ...,
-    // video: ...,
-  },
+  // disable: process.env.NODE_ENV === 'development',
+  // disable: false,
+  // register: true,
+  // skipWaiting: true,
+  // sw: "/sw.js",
+  // buildExcludes: [/middleware-manifest.json$/],
+  // cacheOnFrontEndNav: true,
+  // reloadOnOnline: true,
+  // fallbacks: {
+  //   //image: "/static/images/fallback.png",
+  //   document: "/offline", // if you want to fallback to a custom page rather than /_offline
+  //   // font: '/static/font/fallback.woff2',
+  //   // audio: ...,
+  //   // video: ...,
+  // },
 });
 
 export default withPWA({
